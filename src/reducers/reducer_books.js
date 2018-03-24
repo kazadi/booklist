3a09252a295a81d22033e9ac6c797c59d715008b
kazadi <kazadix2@gmail.com>
2018-03-24 16:58:53 -0500

    finished booklist. added Book Detail component.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'The Dark Tower' },
-        { title: 'The Laundrymen' },
-        { title: 'Richest Man in Babylon' },
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'The Dark Tower', pages: 390 },
+        { title: 'The Laundrymen', pages: 485 },
+        { title: 'Richest Man in Babylon', pages: 80 },
     ];
 }
